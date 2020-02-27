3046940bad9394e49690f289d0fd9a6ba3c59a9d
Ryan Hubley <rhubley25@gmail.com>
2020-02-26 21:55:16 -0500

    added gradle files to front end

---
+++ TheProject/src/main/java/model/CourseAssistant.java
@@ -0,0 +1,34 @@
+package model;
+
+public class CourseAssistant implements Person {
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public void setEmail(String email) {
+
+    }
+}
